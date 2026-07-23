c88f2ad47d6fcaffee8f2dd82c37b51139e6f8e6
Matt Williams <Matt.Williams+amdeng@amd.com>
2026-07-23 02:34:59 -0400

    Adding official Hyperloom header

--- docs/conf.py
+++ docs/conf.py
@@ -8,21 +8,7 @@ version_number = "0.2.0"
 
 html_theme = "rocm_docs_theme"
 html_theme_options = {
-    "flavor": "generic",
-    "header_title": f"Magpie {version_number}",
-    "header_link": False,
-    "version_list_link": False,
-    "nav_secondary_items": {
-        "Hyperloom": "https://rocm.docs.amd.com/projects/hyperloom/en/latest/index.html",
-        "GitHub": "https://github.com/AMD-AGI/Magpie",
-        "Community": False,
-        "Blogs": "https://rocm.blogs.amd.com/",
-        "ROCm Developer Hub": "https://www.amd.com/en/developer/resources/rocm-hub.html",
-        "Instinct™ Docs": "https://instinct.docs.amd.com/",
-        "Infinity Hub": "https://www.amd.com/en/developer/resources/infinity-hub.html",
-        "Support": "https://github.com/AMD-AGI/Magpie/issues/new/choose",
-    },
-    "link_main_doc": False,
+    "flavor": "hyperloom",
 }
 
 # This section turns on/off article info
